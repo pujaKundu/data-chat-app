8cc3af68144ce2d419d8f8fd28f8807b8a9541d5
pujaKundu <iampujakundu@gmail.com>
2022-09-18 22:43:44 +0600

    first commit

---
+++ db.js
@@ -0,0 +1,9 @@
+const users = require('./data/users.json');
+const conversations = require("./data/conversations.json");
+const messages = require("./data/messages.json");
+
+module.exports = {
+  users,
+  conversations,
+  messages,
+};
